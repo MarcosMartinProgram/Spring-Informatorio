6fdebc636b821c102095c3415d0f0062d299e395
marcos <martinmarcospablo@gmail.com>
2023-07-22 14:24:29 -0300

    Conectado de base de datos

--- primera-app/src/main/java/com/info/primeraapp/domain/Book.java
+++ primera-app/src/main/java/com/info/primeraapp/domain/Book.java
@@ -1,9 +1,16 @@
 package com.info.primeraapp.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
@@ -11,9 +18,20 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name="UUID",strategy="org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(name = "id", length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID uuid;
+
+    @Column(length = 40, columnDefinition = "varchar(40)", updatable = true, nullable = false )
     private String title;
+
+    @Column(length = 40, columnDefinition = "varchar(40)", updatable = true, nullable = false)
     private String author;
 
 
